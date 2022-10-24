53c14e79bd868af8fb99445e2f480b047a54ab2f
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-25 03:45:10 +0600

    Signup page design added

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -12,14 +12,15 @@ const SignIn = () => {
         <form className="mt-6">
           <div>
             <label
-              htmlFor="username"
+              htmlFor="email"
               className="block text-sm text-gray-800 dark:text-gray-200"
             >
-              Username
+              Email
             </label>
             <input
-              type="text"
+              type="email"
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              required
             />
           </div>
 
@@ -39,6 +40,7 @@ const SignIn = () => {
             <input
               type="password"
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              required
             />
           </div>
 
